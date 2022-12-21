cb31be6dd3b235dec714ac47f2bffb16e1a95a66
somnia143 <beemakesbzz@yandex.ru>
2022-12-21 16:16:57 +0300

    Initial Commit

---
+++ firstTastDex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstTastDex
+//
+//  Created by Максим Миронов on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
