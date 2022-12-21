6a0f69b04d1f51a5b81a7cb4b227724a91c83168
somnia143 <beemakesbzz@yandex.ru>
2022-12-21 17:22:01 +0300

    Добавление кнопки-счетчика

--- firstTastDex/ViewController.swift
+++ firstTastDex/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var countTouch = 42
+    @IBOutlet weak var text: UIButton!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func textButtonDown(_ sender: Any) {
+        countTouch += 1
+        label.text = ("Значение счетчика: \(countTouch)")
+    }
 }
 
